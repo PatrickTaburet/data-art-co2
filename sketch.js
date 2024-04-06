615ce62e332486ac70fb6fc7eaad1b88718db4b7
Patrick Taburet <taburet.patrick@gmail.com>
2024-04-06 11:59:41 +0200

    Implement input to manage scene.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
+function preload(){
+    table = loadTable("/historical_emissions.csv", "csv", "header")
+}
+let isAnimated = false;
 let table;
 let countryNamesObj = {};
 let dataObj ={};
 let circleDiam;
+let i = 1;
+let countriesArray = []
+let originArray = ["top", "top", "right", "right", "bottom", "bottom", "left", "left"]
+let offset;
+let submitButton = document.querySelector('#submitButton');
+let reloadButton = document.querySelector('#reloadButton');
 
-function preload(){
-    table = loadTable("/historical_emissions.csv", "csv", "header")
-}
-
+// "France", "Ghana", "Spain", "Russia", "Mexico", "China", "Australia", "Japan"
 function setup(){
     createCanvas(800, 800);
-    console.log(width);
+    // console.log(width);
     
     // width = 800 - (800 - 600) / 2; // Set the width to 600 pixels with padding on each side
     // height = 800 - (800 - 600) / 2;
@@ -18,6 +25,18 @@ function setup(){
     colorMode(HSB);
     background(0, 0, 50);
     frameRate(60);
+    submitButton.addEventListener('click', function() {
+        countriesArray = [];
+        document.querySelectorAll('.country').forEach(function(select) {
+          countriesArray.push(select.value);
+        });
+        runAnimation();
+      console.log(isAnimated + "  -  " + countriesArray);
+      
+    });
+    reloadButton.addEventListener('click', function() {
+        window.location.reload();
+    });
     let countries = table.rows;
     for (let i=0 ; i < countries.length ; i++){
         let countriesName = countries[i].arr[1];
@@ -31,6 +50,7 @@ function setup(){
             dataObj[key] = averageValues (dataObj[key])          
         }
     }
+
     // console.log(dataObj["China"]);
     // CHIcircleDiam = dataObj["Chine"];
 
@@ -39,58 +59,77 @@ function setup(){
 }
 // ------- - - - - - - - - - - - - - - - - - -
 // 30 ans de data
-let i = 1;
 function draw(){
     // ------ Offset between 1 et 2000 -------
-    drawData("Brazil", i, 100, "top")
-    drawData("South Korea", i, 300, "top")
+    // chooseDisplay(countriesArray, originArray)
+    console.log(isAnimated);
+
+    if (isAnimated){
+        countriesArray[0] != "" ? drawData(countriesArray[0], i, 100, originArray[0]) : null;
+        countriesArray[1] != "" ? drawData(countriesArray[1], i, 300, originArray[1]): null;
+        countriesArray[2] != "" ? drawData(countriesArray[2], i, 500, originArray[2]): null;
+        countriesArray[3] != "" ? drawData(countriesArray[3], i, 700, originArray[3]): null;
+        countriesArray[4] != "" ? drawData(countriesArray[4], i, 900, originArray[4]): null;
+        countriesArray[5] != "" ? drawData(countriesArray[5], i, 1100, originArray[5]): null;
+        countriesArray[6] != "" ? drawData(countriesArray[6], i, 1300, originArray[6]): null;
+        countriesArray[7] != "" ? drawData(countriesArray[7], i, 1500, originArray[7]): null;
     
-    drawData("Saudi Arabia", i, 600, "top")
-    drawData("Australia", i, 1000, "bottom")
-    drawData("China", i, 800, "left")
-    drawData("Ghana", i, 80, "top")
+        // drawData("South Korea", i, 200, "top")
+        
+        // drawData("Saudi Arabia", i, 300, "top")
+        // drawData("Australia", i, 500, "bottom")
+        // drawData("China", i, 800, "left")
+        // drawData("Ghana", i, 1000, "top")
 
-    drawData("Indonesia", i, 50, "right")
-     drawData("Nigeria", i, 50, "right")
-     drawData("Japan", i, 150, "left")
-    drawData("Russia", i, 200, "bottom")
-    i++
-    if (i == 250){
-        noLoop();
+        // drawData("Indonesia", i, 1200, "right")
+        //  drawData("Japan", i, 1500, "right")
+        //  drawData("Nigeria", i, 1700, "left")
+        // drawData("Russia", i, 2000, "bottom")
+        i++
+        if (i == 250){
+            noLoop();
+        }
+        // console.log(FRcircleDiam[0] + "  +  " +  FRcircleDiam[240]);
+        // console.log(color);
+
+        // ------ LOOPING ------ :
+        // if (i == 240){
+        //     i=0;
+        // }
     }
-    // console.log(FRcircleDiam[0] + "  +  " +  FRcircleDiam[240]);
-    // console.log(color);
-   
-  
-   
-    // ------ LOOPING ------ :
-    // if (i == 240){
-    //     i=0;
-    // }
-   
-
+    
 }
+function runAnimation() {
+    isAnimated = !isAnimated;
+}
+  
 
+
+function handleSubmit(){
+    for (let i = 0; i < 8; i++) {
+        countriesArray.push(select('#country' + i).value());
+      }
+}
 function drawData(country, i, offset, origin){
     let x;
     let y;
     let txtX;
     let txtY;
-    let posOffset = map(offset, 0, 1000, 0, 500)
-    console.log("--->" + posOffset); 
+    let posOffset = map(offset, 0, 2000, 0, 4000)
+    // console.log("--->" + posOffset); 
     switch (origin) {
         case "left":
             x = 0 + width*(i/250);
             y = map(noise(posOffset + 0.01*i), 0, 1, 0, height)
             txtX = x + 25;
             txtY = y-40 ;
-            console.log("left : " + y);
+            // console.log("left : " + y);
             break;
         case "right":
             x = width - width*(i/250);
             y = map(noise( posOffset +0.01*i) , 0, 1, 0, height)
-            txtX = x-150 ;
-            txtY = y -50 ;
+            txtX = x-110 ;
+            txtY = y -40 ;
             // console.log(txtY)
             break;
         case "bottom":
@@ -113,9 +152,8 @@ function drawData(country, i, offset, origin){
     circleDiam = dataObj[country];
     let color = map(circleDiam[i], -50, 10297, 100 + colorOffset , 360 )
     fill(color, 100, 100)
-    console.log(frameCount )
     setTimeout(() => {
-        if(frameCount<56){
+        if(i<2){
             push()
             fill(color, 100, 100)
             strokeWeight(4);
